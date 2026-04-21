6afd965ca5061e27d73cf2b4e0bb1304da830b03
jonatan <jonatan@localhost.localdomain>
2026-04-20 23:01:49 -0600

    WIP

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, AutoModel
+from transformers.models.auto import configuration_auto
+from huggingface_hub import login
+import os
 
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", dtype="auto", device_map="auto")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+login(token = os.environ["HF_TOKEN"])
+
+print(configuration_auto.CONFIG_MAPPING.items())
+
+# This will AUTOMATICALLY download the model from Hugging Face!
+# No need to manually download anything!
+model_name = "chiedo/hello-world"  # Replace with your actual model name
+
+print("Downloading model... (this happens only once)")
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+
+# Test the model
+output = model.generate_hello_world()
+print(output)  # "Hello World!"
+
 
