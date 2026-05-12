1945fb566caafacd000b1001434f0849deeab92b
jonatan <jonatan@localhost.localdomain>
2026-05-11 22:42:43 -0600

    using gemma-4-E2B

--- run.py
+++ run.py
@@ -4,12 +4,20 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, AutoMo
 from transformers.models.auto import configuration_auto
 from huggingface_hub import login
 import os
+from time import sleep
+import transformers
+
+print(transformers.__version__)
 
 login(token = os.environ["HF_TOKEN"])
 
 from transformers import pipeline
 
-qa_model = pipeline("text-generation", model="HuggingFaceTB/SmolVLM-256M-Instruct")
+model = "google/gemma-4-E2B"
+
+qa_model = pipeline("text-generation", model=model)
 qam = qa_model(text_inputs="what is the meaning of life?")
 
 print(qam)
+
+sleep(1000000)
